55275b33182ce2c2327bd4fa42dd7f9b04e459b3
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-05-07 22:32:54 +0600

    added 4 methods

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 let db = null;
+
 const connectdb = async()=>{
     if(db) return db;
      const client = new MongoClient(process.env.DB_URL, {
@@ -13,10 +14,16 @@ const connectdb = async()=>{
 
     await client.connect();
     client.db("admin").command({ ping: 1 });
-    console.log("✅ DB connected successfully");
-
     db = client.db("simpleCrud");
     return db;
 }
 
-module.exports = connectdb;
+/**
+ * @returns {import('mongodb').Collection}  //for suggestion
+ */
+
+const getUsersCollection = async () => {
+    const database = await connectdb();
+    return database.collection("users"); 
+};
+module.exports = { connectdb, getUsersCollection };
--- server.js
+++ server.js
@@ -5,34 +5,91 @@ const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config();
 const dns = require('dns');
-const connectdb = require('./db/dbConnection');
+
+const { connectdb, getUsersCollection } = require('./db/dbConnection.js');
+const e = require('express');
+
 dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 app.get('/', (req, res)=>{
     res.send({status: "success", message: "server is running"});
 })
 
-app.use(async (req, res, next)=>{
-    try {
-        req.db = await connectdb();
-        next();
-    } catch (error) {
-         res.status(500).send({ status: "error", message: "DB connection failed" });
-    }
-})
+connectdb()
+    .then(() => console.log("✅ DB connected successfully"))
+    .catch((err) => {
+        console.log("❌ DB connection failed", err);
+        process.exit(1); // DB connect না হলে server বন্ধ করো
+    });
 
 app.get("/users", async(req, res)=>{
-    const userCollection = req.db.collection("users");
-    const cursor = await userCollection.find();
+    const usersCollection = await getUsersCollection();
+    const cursor = await usersCollection.find();
     const users = await cursor.toArray();
     res.send({data: users})
 })
+
+app.get("/users/:id", async(req, res)=>{
+    const id = req.params.id;
+    const usersCollection = await getUsersCollection();
+    const query = {_id: new ObjectId(id)};
+    const user = await usersCollection.findOne(query);
+    if(user){
+        res.send({data: user});
+    } else {
+        res.status(404).send({status: "error", message: "User not found"});
+    }
+})
+
+
+app.post("/createuser", async(req, res)=>{
+    const newUser = req.body;
+    const usersCollection = await getUsersCollection();
+    const result = await usersCollection.insertOne(newUser);
+    console.log(result.acknowledged);
+    if(result.acknowledged){
+        res.send({status: "success", message: "User created successfully", data: result})
+    } else {
+        res.status(500).send({status: "error", message: "Failed to create user"})
+    }
+})
+
+app.patch("/updateuser/:id", async(req, res)=>{
+    const id = req.params.id;
+    const updatedData = req.body;
+    const usersCollection = await getUsersCollection();
+    const query = {_id: new ObjectId(id)};
+    const updatableUser = {
+        $set: {
+            name: updatedData.name,
+            email: updatedData.email,
+            role: updatedData.role
+        }
+    }
+    const result = await usersCollection.updateOne(query, updatableUser);
+    if(result.modifiedCount > 0){
+        res.send({status: "success", message: "User updated successfully", data: result})
+    } else {
+        res.status(500).send({status: "error", message: "Failed to update user"})
+    }
+})
+
+app.delete("/deleteuser/:id", async(req, res)=>{
+    const id = req.params.id;
+    const usersCollection = await getUsersCollection();
+    const query = {_id: new ObjectId(id)};
+    const result = await usersCollection.deleteOne(query);
+    if(result.deletedCount > 0){
+        res.send({status: "success", message: "User deleted successfully", data: result})
+    } else {
+        res.status(500).send({status: "error", message: "Failed to delete user"})
+    }
+})
+
 
 app.listen(port, ()=>{
     console.log(`server is running on port http://localhost:${port}`);
